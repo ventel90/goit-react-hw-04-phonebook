6869b2e78b2c704a2e5dd42d76d986d5fb940f31
VItaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-01-31 17:37:53 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,28 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import { Container } from './App.styled'
+import { Container } from './App.styled';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './FilterContacts/FilterContacts';
-import ContactList from './ContactList/ContactList'
+import ContactList from './ContactList/ContactList';
 
 export const App = () => {
- const [contacts, setContacts] = useState([
-   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
- ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
 
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const contactsStorage = JSON.parse(localStorage.getItem('contacts'));
-    if (contactsStorage) {
-      setContacts(contactsStorage);
-    }
-},[])
+  //   useEffect(() => {
+  //     const contactsStorage = JSON.parse(localStorage.getItem('contacts'));
+  //     if (contactsStorage) {
+  //       setContacts(contactsStorage);
+  //     }
+  // },[])
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
@@ -38,23 +40,21 @@ export const App = () => {
       setContacts([contact, ...contacts]);
     }
   };
- 
 
   const changeFilter = event => {
     setFilter(event.currentTarget.value);
   };
 
   const deleteContact = contactId => {
-    setContacts(contacts.filter(contact => contact.id !== contactId),
-    );
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
   const normalizeFilter = filter.toLowerCase();
-      const filterContacts = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(normalizeFilter)
-);
-  
-return (
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizeFilter)
+  );
+
+  return (
     <Container>
       <h1>PhoneBook</h1>,
       <ContactForm onSubmit={addContact} />,
@@ -63,22 +63,7 @@ return (
       <ContactList contacts={filterContacts} onDeleteContact={deleteContact} />,
     </Container>
   );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 // export class OldApp extends Component {
 //   state = {
@@ -109,9 +94,9 @@ return (
 //     }
 //   };
 
-  // changeFilter = event => {
-  //   this.setState({ filter: event.currentTarget.value });
-  // };
+// changeFilter = event => {
+//   this.setState({ filter: event.currentTarget.value });
+// };
 
 //   handleChange = event => {
 //     const { name, value } = event.currentTarget;
